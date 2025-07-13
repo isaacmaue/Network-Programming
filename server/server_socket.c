d833eca90a957ce7f78edf7bb718b01d59edb766
isaac <isaac.maue@protonmail.com>
2025-07-13 12:52:35 +0000

    Added print statements to show connection status.

--- server/server_socket.c
+++ server/server_socket.c
@@ -2,8 +2,10 @@
 /* To build this socket, we will follow this workflow:
 1. Create the cosket
 2. Bind the socket to the IP and port 
-3. Listen for incoming connections 
+3. Listen for incoming connections from a client
 4. Accept the incoming connections
+5. Send data 
+6. close the socket
 */
 
 #include <stdio.h>
@@ -15,22 +17,39 @@
 #include <unistd.h>
 
 int main(){
+	//Message to send to the client upon successful connection
 	char server_message [256] = "Connected to the server.";	
 
-
+	//The main server socket
 	int server_socket ;
+	//Assign socket type to server socket. We are using IPV4 , TCP, and 0 for protocol spec
 	server_socket = socket (AF_INET, SOCK_STREAM, 0 );
+
 	
+	//Fill out server struct sockaddr_in. Famil, port, and IP
 	struct sockaddr_in server_address;
 	server_address.sin_family = AF_INET;
 	server_address.sin_port = htons(9002);
 	server_address.sin_addr.s_addr = inet_addr("192.168.1.52");
+	
+
 
 	//Bind the socket 
-	bind(server_socket, (struct sockaddr *) &server_address, sizeof(server_address));
+	int bind_result = bind(server_socket, (struct sockaddr *) &server_address, sizeof(server_address));
+	if (bind_result == 0){
+		printf("Binding was successful\n");
+		}else{
+		printf("Binding failed\n.");
+	}
+
 
 	//Listen for connections
-	listen(server_socket, 1);
+	int listen_result = listen(server_socket, 1);
+	if(listen_result == 0){
+	printf("Listen successful\n.");
+	}else{
+		printf("Listen failed\n.");
+	}
 	
 	
 	struct sockaddr_in client_address;
